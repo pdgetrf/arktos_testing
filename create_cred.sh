7a9242b3d5c2dc77b36cd1b0f723cdf1dadc75f6
root <root@ip-172-31-31-58.us-west-2.compute.internal>
2020-04-03 00:56:32 +0000

    updates

--- create_cred.sh
+++ create_cred.sh
@@ -9,19 +9,24 @@ then
 fi
 
 user=$1
-namespace="$1namespace"
 
-tenant_name="$1tenant"
-tenant_name2="${tenant_name}xyz"
-group_name="$1group"
-context_name="$1-context"
+tenant_name="${user}tenant"
+
+namespace="${user}-namespace"
+
+group_name="${user}-group"
+context_name="${user}-context"
 
 echo "create cert for tenant $tenant_name"
 
-set -x
-kubectl create ns $namespace
-openssl genrsa -out $user.key 2048
-openssl req -new -key $user.key -out $user.csr -subj "/CN=$user/O=tenant:$tenant_name/OU=$group_name"
-openssl x509 -req -in $user.csr -CA /var/run/kubernetes/client-ca.crt -CAkey /var/run/kubernetes/client-ca.key -CAcreateserial -out $user.crt -days 500
+kubectl create tenant $tenant_name 
+kubectl create ns $namespace --tenant $tenant_name
+
+# create certificate for user
+openssl genrsa -out $user.key 2048 # client key
+openssl req -new -key $user.key -out $user.csr -subj "/CN=$user/O=tenant:$tenant_name/OU=$group_name" # create sign request
+openssl x509 -req -in $user.csr -CA /var/run/kubernetes/client-ca.crt -CAkey /var/run/kubernetes/client-ca.key -CAcreateserial -out $user.crt -days 500 # create cert signed by CA
+
+# set user in context
 kubectl config set-credentials $user --client-certificate=/root/$user.crt --client-key=/root/$user.key
-kubectl config set-context $context_name --cluster=local-up-cluster --namespace=$namespace --user=$user --tenant=$tenant_name2
+kubectl config set-context $context_name --cluster=local-up-cluster --namespace=$namespace --user=$user --tenant=$tenant_name
